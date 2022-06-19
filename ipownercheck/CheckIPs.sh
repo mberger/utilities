bd72b36b76cd778d3e6806f6b8d3d8a9e5e87954
Michael Berger <mberger@internetinnovation.com>
2022-06-18 22:35:44 -0400

    created new utilities dir

--- ipownercheck/CheckIPs.sh
+++ ipownercheck/CheckIPs.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 # usage: $0 &lt;domains file&gt;
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+WHITE='\033[0;37m'
+RESET='\033[0m'
+
 domains=`cat $1`
 for domain in $domains
 do
     stripped_domain=${domain#www.} # strip a leading www.
     reg=`whois $stripped_domain | grep -i  Organization:`
-    echo "[$domain] $reg"
-done
+    
+  if [[ $reg == "Amazon" ]]; then
+    echo -e "[$domain] $reg"
+  else
+    echo -e "[$domain] $reg"
+  fi
+  done
